c620b2ce1fa36b3952ed0b44cabfd5d8ca4596ad
DandeVamshiVardhan <vamshivamshi3452@gmail.com>
2024-08-26 13:53:19 +0530

    Add files via upload

---
+++ Debug/src/helloworld.d
@@ -0,0 +1,36 @@
+src/helloworld.o src/helloworld.o: ../src/helloworld.c ../src/platform.h \
+ ../src/platform_config.h \
+ ../../convol_test_bsp/microblaze_0/include/xil_printf.h \
+ ../../convol_test_bsp/microblaze_0/include/xil_types.h \
+ ../../convol_test_bsp/microblaze_0/include/xparameters.h \
+ ../../convol_test_bsp/microblaze_0/include/bspconfig.h \
+ ../../convol_test_bsp/microblaze_0/include/xil_io.h \
+ ../../convol_test_bsp/microblaze_0/include/xil_printf.h \
+ ../../convol_test_bsp/microblaze_0/include/mb_interface.h \
+ ../../convol_test_bsp/microblaze_0/include/xil_assert.h \
+ ../../convol_test_bsp/microblaze_0/include/xil_exception.h \
+ ../../convol_test_bsp/microblaze_0/include/xparameters.h
+
+../src/platform.h:
+
+../src/platform_config.h:
+
+../../convol_test_bsp/microblaze_0/include/xil_printf.h:
+
+../../convol_test_bsp/microblaze_0/include/xil_types.h:
+
+../../convol_test_bsp/microblaze_0/include/xparameters.h:
+
+../../convol_test_bsp/microblaze_0/include/bspconfig.h:
+
+../../convol_test_bsp/microblaze_0/include/xil_io.h:
+
+../../convol_test_bsp/microblaze_0/include/xil_printf.h:
+
+../../convol_test_bsp/microblaze_0/include/mb_interface.h:
+
+../../convol_test_bsp/microblaze_0/include/xil_assert.h:
+
+../../convol_test_bsp/microblaze_0/include/xil_exception.h:
+
+../../convol_test_bsp/microblaze_0/include/xparameters.h:
---
+++ Debug/src/platform.d
@@ -0,0 +1,25 @@
+src/platform.o src/platform.o: ../src/platform.c \
+ ../../convol_test_bsp/microblaze_0/include/xparameters.h \
+ ../../convol_test_bsp/microblaze_0/include/xil_cache.h \
+ ../../convol_test_bsp/microblaze_0/include/mb_interface.h \
+ ../../convol_test_bsp/microblaze_0/include/xil_types.h \
+ ../../convol_test_bsp/microblaze_0/include/xil_assert.h \
+ ../../convol_test_bsp/microblaze_0/include/xil_exception.h \
+ ../../convol_test_bsp/microblaze_0/include/xparameters.h \
+ ../src/platform_config.h
+
+../../convol_test_bsp/microblaze_0/include/xparameters.h:
+
+../../convol_test_bsp/microblaze_0/include/xil_cache.h:
+
+../../convol_test_bsp/microblaze_0/include/mb_interface.h:
+
+../../convol_test_bsp/microblaze_0/include/xil_types.h:
+
+../../convol_test_bsp/microblaze_0/include/xil_assert.h:
+
+../../convol_test_bsp/microblaze_0/include/xil_exception.h:
+
+../../convol_test_bsp/microblaze_0/include/xparameters.h:
+
+../src/platform_config.h:
